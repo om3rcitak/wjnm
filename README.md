e4ad2461d113203149c0c369682fc2be0eacb251
Ömer Çıtak <mail@omercitak.com>
2017-04-17 15:27:08 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# World Job Network Map
+
+This map lists people who work in different countries around the world and want to help developers who want to move to those countries.
+
+## How to add new people?
+Ez, just send pull-request to [data.json](https://github.com/Om3rCitak/wjnm/edit/master/data.json) file :)
---
+++ wjnm.js
@@ -0,0 +1,55 @@
+/**
+* World Job Network Map
+*
+* Omer Citak - www.omercitak.com - @Om3rCitak
+*
+*/
+
+jQuery.noConflict();
+jQuery(function(){
+	var $ = jQuery;
+
+	var height = $(document).height();  
+	$('#map').height(height);
+	$('#results').height(height);
+
+	$.getJSON("data.json").done(function (data) {
+		var countries = {};
+		
+		$.each(data, function(key, val) {
+			countries[key] = 1;
+        });
+		
+		$('#map').vectorMap({
+			panOnDrag: true,
+			onRegionClick: function(event, code){           
+				var name = (code);
+				listPeoples(data[code]);
+			},
+			series: {
+				regions: [{
+					scale: ['#0071A4'],
+					normalizeFunction: 'polynomial',
+					values: countries
+				}]
+			}
+		});
+	});
+})
+
+function listPeoples(data){
+	var $ = jQuery;
+	var list = "<ul>";
+	
+	if(data){
+		$.each(data, function(key, val) {
+			list = list + '<li>' + val + '</li>';
+		});
+	}else{
+		list = list + '<li> :( none. <a href="https://github.com/Om3rCitak/wjnm/edit/master/data.json">add?</a></li>';
+	}
+	
+	list = list + '</ul>';
+	
+	$("#list").html(list);
+}
